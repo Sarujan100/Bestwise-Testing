d501fc0e57bf53e2332984edd353cc9d61f20d5e
ReyolJanu <ignatious.janukshan20@gmail.com>
2025-05-31 10:49:08 +0530

    setup CSS variables

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function page() {
   return (
     <div className='bg-background'>
-      <span className='font-large'>hello</span>
+      <span className='font-large btn-color'>helloff</span>
     </div>
+
   )
 }
 
